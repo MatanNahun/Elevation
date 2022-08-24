5f95ecacc8a6c9a66a27172bbb64c4a7b3c8ee55
MatanNahum <matan.nahum95@gmail.com>
2022-08-24 12:36:21 +0300

    materialize-css

--- Exercises/week-2/Tweeter-Logic-Module/tweeterRender.js
+++ Exercises/week-2/Tweeter-Logic-Module/tweeterRender.js
@@ -4,12 +4,12 @@ const tweeterRender = function () {
   const renderPosts = function (posts) {
     $("#posts").empty();
     for (const post of posts) {
-      let postToAppend = `<div class="post post-text" id="${post.id}"> ${post.text} </div>`;
+      let postToAppend = `<div class="post post-text" id="${post.id}"> ${post.text} <button class="delete">delete</button> <div><input type="text" class="inputComment" placeHolder="comment here"> <button class="addCommentButton">add comment</button>  </div> </div>`;
       // console.log(postToAppend);
       $("#posts").append(postToAppend);
 
       for (let comment of post.comments) {
-        let commentToAppend = `<div class="comments" id="${comment.id}"> ${comment.text} </div>`;
+        let commentToAppend = `<div class="comments" id="${comment.id}"> ${comment.text}  <button class="delete-comment">delete</button> </div>`;
         //   console.log(commentToAppend);
         $(`#${post.id}`).append(commentToAppend);
       }
