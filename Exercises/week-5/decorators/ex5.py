1c7e604af541f37ea3e2f610da7dda1d29443468
MatanNahum <matan.nahum95@gmail.com>
2022-09-15 18:57:33 +0300

    decorators - done

--- Exercises/week-5/decorators/ex5.py
+++ Exercises/week-5/decorators/ex5.py
@@ -1,6 +1,27 @@
+from time import sleep
+
+
+def cache_decorator(some_func):
+    def inner(*args, **kwargs):
+        if inner.cash.get(args[0]):
+            res = inner.cash.get(args[0])
+        else:
+            res = some_func(*args, **kwargs)
+            inner.cash[args[0]] = res
+            sleep(4)
+        return res
+
+    inner.cash = {}
+    return inner
+
+
+@cache_decorator
 def fibonacci(num):
-    print("calculating: ", num)
+    if num in {0, 1}:  # Base case
+        return num
+    return fibonacci(num - 1) + fibonacci(num - 2)  # Recursive case
 
 
 print(fibonacci(2))
 print(fibonacci(4))
+print(fibonacci(2))
