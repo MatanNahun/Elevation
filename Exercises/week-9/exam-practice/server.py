80f7355d3b9518b20a33b4a84ae43bec6d11dbd6
MatanNahum <matan.nahum95@gmail.com>
2022-10-20 10:51:08 +0300

    inital full stack program

---
+++ Exercises/week-9/exam-practice/server.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from fastapi import Request, Response
+import requests
+import uvicorn
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+app.mount("/client", StaticFiles(directory="client"), name="client")
+
+
+@app.get("/")
+def root():
+    return FileResponse("index.html")
+
+
+@app.get("/sanity")
+def sanity():
+    return {"message": "Server is up and running in sanity"}
+
+
+@app.get("/name/{username}")
+def greet_user(username):
+    return {f"message:Hi there {username}"}
+
+
+# localhost:3000/name?name=robert
+@app.get("/name")
+def greet_user(name):
+    return {f"message:Hi there {name}"}
+
+
+if __name__ == "__main__":
+    uvicorn.run("server:app", host="0.0.0.0", port=8020, reload=True)
