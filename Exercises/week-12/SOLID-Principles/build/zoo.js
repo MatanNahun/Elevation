7c96528b7db445760a5f5ff89a669251f0db4e3b
MatanNahum <matan.nahum95@gmail.com>
2022-11-03 20:19:14 +0200

    zoo refill

--- Exercises/week-12/SOLID-Principles/build/zoo.js
+++ Exercises/week-12/SOLID-Principles/build/zoo.js
@@ -2,13 +2,20 @@
 class Zoo {
     constructor() {
         this._supply = {};
+        this._supplyToOrder = {
+            meat: 60000,
+            planktons: 2000000000,
+            algae: 33,
+            seeds: 1200,
+            worms: 800,
+        };
         this._animals = [];
         this._supply = {
-            meat: new Food("meat", 1000000),
-            planktons: new Food("plankton", 2000000000),
-            algae: new Food("algae", 1500),
-            seeds: new Food("seeds", 1200),
-            worms: new Food("worms", 1000),
+            meat: new Food("meat", this._supplyToOrder.meat),
+            planktons: new Food("plankton", this._supplyToOrder.planktons),
+            algae: new Food("algae", this._supplyToOrder.algae),
+            seeds: new Food("seeds", this._supplyToOrder.seeds),
+            worms: new Food("worms", this._supplyToOrder.worms),
         };
     }
     addAnimalToZoo(animal) {
@@ -19,9 +26,18 @@ class Zoo {
     }
     dailyfeed() {
         this._animals.forEach((animal) => {
-            console.log(animal.feed());
             let animalFood = animal.food;
+            this.refillIfNeeded(animalFood);
             this.removeFromSupply(animalFood);
+            console.log(animal.feed());
         });
+    }
+    refillIfNeeded(animalFood) {
+        if (this._supply[animalFood.type].dailyQuantityGrams <
+            animalFood.dailyQuantityGrams) {
+            this._supply[animalFood.type].dailyQuantityGrams +=
+                this._supplyToOrder[animalFood.type];
+            console.log(`refill for ${animalFood.type} was done`);
+        }
     }
 }
--- Exercises/week-12/SOLID-Principles/src/Zoo.ts
+++ Exercises/week-12/SOLID-Principles/src/Zoo.ts
@@ -1,15 +1,22 @@
 class Zoo {
   private _animals: Animal[];
   private _supply: any = {};
+  private _supplyToOrder: any = {
+    meat: 60000,
+    planktons: 2000000000,
+    algae: 33,
+    seeds: 1200,
+    worms: 800,
+  };
 
   constructor() {
     this._animals = [];
     this._supply = {
-      meat: new Food("meat", 1000000),
-      planktons: new Food("plankton", 2000000000),
-      algae: new Food("algae", 1500),
-      seeds: new Food("seeds", 1200),
-      worms: new Food("worms", 1000),
+      meat: new Food("meat", this._supplyToOrder.meat),
+      planktons: new Food("plankton", this._supplyToOrder.planktons),
+      algae: new Food("algae", this._supplyToOrder.algae),
+      seeds: new Food("seeds", this._supplyToOrder.seeds),
+      worms: new Food("worms", this._supplyToOrder.worms),
     };
   }
 
@@ -23,9 +30,21 @@ class Zoo {
 
   dailyfeed() {
     this._animals.forEach((animal) => {
-      console.log(animal.feed());
       let animalFood = animal.food;
+      this.refillIfNeeded(animalFood);
       this.removeFromSupply(animalFood);
+      console.log(animal.feed());
     });
+  }
+
+  refillIfNeeded(animalFood: Food) {
+    if (
+      this._supply[animalFood.type].dailyQuantityGrams <
+      animalFood.dailyQuantityGrams
+    ) {
+      this._supply[animalFood.type].dailyQuantityGrams +=
+        this._supplyToOrder[animalFood.type];
+      console.log(`refill for ${animalFood.type} was done`);
+    }
   }
 }
